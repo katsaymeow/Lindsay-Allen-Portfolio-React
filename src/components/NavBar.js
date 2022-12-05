21b7cdb383470cec6f5c69080d5ec52e9c5c6760
katsaymeow <107221758+katsaymeow@users.noreply.github.com>
2022-12-05 16:58:16 -0500

    navbar work

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,32 @@ import React from 'react';
 
 const navStyle = {
     card: {
-        margin: 20,
-        minHeight: 40,
+        margin: 15,
+        minHeight: 50,
         fontSize: '2rem',
         color: '#414B3B',
         background: '#DFEDD6',
         fontFamily: 'ui-monospace',
         alignContent: 'center',// why wont this work!!
     },
+    links: {
+        margin: 10,
+        padding: '10px',
+    }
 };
 
+const resume = './Resume';
+const interest = './Interest';
+const mainPage = './MainPage';
 export default function NavBar() {
     return (
-        <div style={navStyle.card}>#test</div>
+        <div style={navStyle.card}>
+            <div className='links'>
+          <a href={resume} style={navStyle.links}>Resume</a> 
+          <a href={interest} style={navStyle.links}>Interest</a> 
+          <a href={mainPage} style={navStyle.links}>Main</a>
+          </div>
+        </div>
     );
 };
 // needs to have links to the Interest page and resume
